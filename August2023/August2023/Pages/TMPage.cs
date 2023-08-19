2ba12e34810fa39e9decf67438c72084648856fc
shaistha <shaisthahaneef@gmail.com>
2023-08-19 14:11:57 +0530

    added create new test

---
+++ August2023/August2023/Pages/TMPage.cs
@@ -0,0 +1,81 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace August2023.Pages
+{
+    public class TMPage
+    {
+        public void CreateTimeRecord( IWebDriver driver )
+        {
+            //Create a new time record
+
+
+
+            //click on create new button
+
+            IWebElement CreateNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+            CreateNewButton.Click();
+
+
+            //select time from dropdown
+
+            IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+            typeCodeDropdown.Click();
+
+            IWebElement timeTypeCode = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+            timeTypeCode.Click();
+
+            //enter code
+
+            IWebElement CodeTextbox = driver.FindElement(By.Id("Code"));
+            CodeTextbox.SendKeys("aug");
+
+            //enter dscription
+
+            IWebElement DescriptionTextbox = driver.FindElement(By.Id("Description"));
+            DescriptionTextbox.SendKeys("month");
+
+            //ente price
+
+            IWebElement PriceTexbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+            PriceTexbox.SendKeys("12");
+
+            //click on save button
+
+            IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+            saveButton.Click();
+            Thread.Sleep(5000);
+
+            //check if new time record has been created successfully
+
+            IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+            goToLastPageButton.Click();
+
+            IWebElement newCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+            if (newCode.Text == "aug")
+            {
+                Console.WriteLine("New time record has been created successfully");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Time record has not been created");
+            }
+        }
+       
+        public void EditTimeRecord (IWebDriver driver)
+        {
+            // Code for edit time record
+        }
+
+        public void DeleteTimeRecord (IWebDriver driver)
+        {
+            // Code for delete time record
+        }
+    }
+}
--- August2023/August2023/Program.cs
+++ August2023/August2023/Program.cs
@@ -1,40 +1,39 @@
 ﻿
+using August2023.Pages;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
-//open the browser
-
-IWebDriver driver = new ChromeDriver();
-
-//Launch turnup portal and navigate to login
-
-driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
-driver.Manage().Window.Maximize();
-
-//identify username textbox and enter valid username
-
-IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
-usernameTextbox.SendKeys("hari");
-
-//identify password textbox and enter valid password
-
-IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
-passwordTextbox.SendKeys("123123");
-
-//identify login button and click on theb button
-
-IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
-loginButton.Click();
-
-//check if user has logged in successfully
-
-IWebElement hellohari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
-if (hellohari.Text=="Hello hari!")
+public class Program
 {
-    Console.WriteLine("user logged in successfully");
-    
+    private static void Main(string[] args)
+    {
+        //open the  browser
+
+        IWebDriver driver = new ChromeDriver();
+
+        //login page object initialization and definition
+
+        LoginPage loginPageObj = new LoginPage();
+        loginPageObj.LoginActions(driver);
+
+        //Home page object initialization and definition
+
+        HomePage homePageObj = new HomePage();
+        homePageObj.GoToTMPage(driver);
+
+        //TM page object initialization and definition
+
+        TMPage tmPageObj = new TMPage();
+        tmPageObj.CreateTimeRecord(driver);
+
+        tmPageObj.EditTimeRecord(driver);
+        tmPageObj.DeleteTimeRecord(driver);
+
+
+    }
 }
-else
-{
-    Console.WriteLine("user hasn't logged in successfully");
-}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
