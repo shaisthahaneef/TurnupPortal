db30172199144cf248c59aa99825218a6af52a48
sHAISTHA <shaisthahaneef@gmail.com>
2023-08-28 23:54:36 +0530

    Adding the initial code for specflow

---
+++ August2023/August2023/FeatureFiles/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As a Turnup portal admin
+I would like to create, edit and delete time and material records
+So that I can manage employees time and materials successfully
+
+@regression
+Scenario: Create time record with valid details
+	Given I logged into Turnup portal successfully
+	And I navigate to Time and material page
+	When I create a new time record
+	Then the record should be created successfully
---
+++ August2023/August2023/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace August2023.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged into Turnup portal successfully")]
+        public void GivenILoggedIntoTurnupPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I navigate to Time and material page")]
+        public void GivenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I create a new time record")]
+        public void WhenICreateANewTimeRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
